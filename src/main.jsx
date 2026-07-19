bd12d784e9459ae12d17d0ba355fdefa3d59bbd5
dhruvpatel16120 <dhruvpatel16120@gmail.com>
2026-07-19 19:33:06 +0530

    add vercel anlytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
         <Toaster 
           position="bottom-right"
           toastOptions={{
